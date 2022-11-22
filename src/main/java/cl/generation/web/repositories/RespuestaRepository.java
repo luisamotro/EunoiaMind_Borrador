a3cf56a0c2fe954ca89595f1c67a5998a7cbaa5b
Atenas Pérez <97369170+At3nas@users.noreply.github.com>
2022-11-22 10:52:43 -0300

    Rutas de Emocion

--- src/main/java/cl/generation/web/repositories/RespuestaRepository.java
+++ src/main/java/cl/generation/web/repositories/RespuestaRepository.java
@@ -1,11 +1,12 @@
 package cl.generation.web.repositories;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import org.springframework.stereotype.Repository;
 import cl.generation.web.models.Respuesta;
 
 @Repository
-public interface RespuestaRepository extends JpaRepositoryImplementation<Respuesta, Long> {
+public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
 
 }
